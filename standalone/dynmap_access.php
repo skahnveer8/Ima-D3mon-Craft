77995bd6b9b15145f31f584fd666bbc0ad5f946b
Sailoriae <62501693+Sailoriae@users.noreply.github.com>
2022-09-15 16:35:18 +0200

    Interface de Dynmap Le "index.html" est légèrement modifié Les tuiles vont arriver dans les commits suivants (On ne peut pas push plus de 2 Go sur Github)

---
+++ standalone/dynmap_access.php
@@ -0,0 +1,5 @@
+<?php
+$tilespath = '/home/projets/EagleCraft/Minecraft/plugins/dynmap/../../../Public_HTTP_Dynmap/tiles/';
+$markerspath = '/home/projets/EagleCraft/Minecraft/plugins/dynmap/../../../Public_HTTP_Dynmap/tiles/';
+$webpath = '/home/projets/EagleCraft/Minecraft/plugins/dynmap/../../../Public_HTTP_Dynmap/';
+?>
